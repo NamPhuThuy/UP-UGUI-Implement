02d1f04b1f7ba6e5cdd1b9069a494ff3c1f9dacb
NamPhuThuy <ttnam957@gmail.com>
2025-10-23 00:33:05 +0700

    update

---
+++ GUI/GUIManager.References.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace NamPhuThuy.UI
+{
+    
+    public partial class GUIManager
+    {
+        #region Private Serializable Fields
+        
+        [SerializeField] private GUILevelWin guiLevelWin;
+
+        #endregion
+
+    }
+
+}
